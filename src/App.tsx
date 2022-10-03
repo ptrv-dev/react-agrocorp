505c9aec8fc26f3b09a532af6bb5d39eacad40df
ptrv-dev <petrov99119@gmail.com>
2022-10-03 22:31:14 +0300

    Начало разработки

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+import Home from './pages/Home';
+
+import './App.scss';
 
 const App: React.FC = () => {
-	return <div></div>;
+	return (
+		<BrowserRouter>
+			<Header />
+			<Routes>
+				<Route index element={<Home />} />
+				<Route path="*" element={<div>Not Found</div>} />
+			</Routes>
+			<Footer />
+		</BrowserRouter>
+	);
 };
 
 export default App;
